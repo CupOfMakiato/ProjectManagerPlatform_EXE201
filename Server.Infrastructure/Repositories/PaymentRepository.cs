ec3741c6805e6a421355ccfd581393467c365db3
ungcamtuankiet <ungcamtuankiet94@gmai.com>
2025-04-13 22:30:43 +0700

    fix payment

---
+++ Server.Infrastructure/Repositories/PaymentRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Application.Interfaces;
+using Server.Application.Repositories;
+using Server.Domain.Entities;
+using Server.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Infrastructure.Repositories
+{
+    public class PaymentRepository : GenericRepository<Payment>, IPaymentRepository
+    {
+        private readonly AppDbContext _dbContext;
+        public PaymentRepository(AppDbContext dbContext,
+            ICurrentTime timeService,
+            IClaimsService claimsService)
+        : base(dbContext,
+                  timeService,
+                  claimsService)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddPaymentAsync(Payment payment)
+        {
+            await _dbContext.Payments.AddAsync(payment);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
